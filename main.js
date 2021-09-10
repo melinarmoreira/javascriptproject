1a69cb91dca1c8b2b9482cc50124d06bb9efcf61
Melina Moreira <melina.moreira@live.com.ar>
2021-09-09 22:25:37 -0300

    3 Pre Entrega

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-//render contador
+//Renderizar contador
 
 function renderAccumulator(accumulator) {
     let contadorRender = document.getElementById('contador');
@@ -8,9 +8,22 @@ function renderAccumulator(accumulator) {
                                 </p>`;
 }
 
+function dataPedido () {
 
+    $('.producto-pedido').append(` ${cart.length} PRODUCTOS $${total} `);
+    
+    if (total >= 5000) {
+        $('.entrega-pedido').append('Envío GRATIS ');
+        $('.envio-gratis').append('Tienes envío gratis por tu compra mayor a $5000');
+    }
+    else {
+        $('.entrega-pedido').append('Envío a pagar');
+        $('.envio-gratis').append('Envío gratis solo con compras mayores a $5000');
+    }
 
-// $(document).ready( function() {
+    $('.total-pedido').append(` TOTAL $${total}`);
+    $('.iva-pedido').append(` IVA incluido $${total * 0.21} `);
+}
 
     //Renderizar nombre
 
@@ -26,10 +39,7 @@ function renderAccumulator(accumulator) {
         let pname = document.createElement('p');
         welcome.appendChild(pname);
         pname.innerHTML = `Signed as ${renderUserName.toUpperCase()}`; 
-
     })
-
-// })
 
 //Funcion que renderiza el carrito
 function renderCart (product) {
@@ -51,7 +61,6 @@ function renderCart (product) {
     row.appendChild(td);
 
     td = document.createElement('td');
-    // td.innerHTML = `<button class='delete' value="${product.id}"><img class="deletepic" src="img/deletepng.png"></button>`;
     td.innerHTML = `<img class ="delete" src="img/deletepng.png" type="button" value ="${product.id}">`
     row.appendChild(td);
 
@@ -63,9 +72,6 @@ function renderDelete () {
     let row = document.querySelector('tr');
     cartindex.removeChild(row);
 }
-
-
-
 
 let products = [
 
@@ -96,7 +102,6 @@ products.forEach(product => {
 container.innerHTML = html;
 
 
-
 let cart = [];
 let accumulator = 0;
 console.log(cart, 'Su carrito está vacio');
@@ -105,21 +110,17 @@ let buyButtons = document.getElementsByClassName('button-buy');
     //Bucle botones de comprar
 for (const button of buyButtons) {
 
-
-
         button.addEventListener('click', function (event) {
             //Agregar al carrito productos y renderizarlos
                 let selectedProduct = products.find( x => x.id == event.target.value);
                 cart.push(selectedProduct);
                 renderCart(cart[cart.length - 1]);
                 accumulator ++;
-                
-                
+                //Renderizar el contador 
                 renderAccumulator(accumulator);
 
-
                 //Eliminar productos del cart, y renderizarlo
-                let deleteButton = document.getElementsByClassName("delete");
+                    let deleteButton = document.getElementsByClassName("delete");
 
                     for(const button1 of deleteButton) {
                     
@@ -131,32 +132,30 @@ for (const button of buyButtons) {
 
                                         cart.splice(cart.indexOf(deletedProduct), 1);
                                         e.target.parentNode.parentNode.remove();
-
-
+                                        //Renderizar el contador
                                         accumulator --;
                                         renderAccumulator(accumulator);
-                })}
+                            })
+                    }
         })
-
 }
 
 // Boton de checkout
-// let checkoutButton = document.querySelector('.button-checkout');
+
 let total = 0;
 $(".button-checkout").one( "click", function (e) {
 
-    $(".containergrid").css("filter", "none");
-    $(".form").slideDown(1500);
+    e.preventDefault();
 
         for (let i = 0; i < cart.length; i++) {
             total += cart[i].price;
             console.log(total);
         }
 
-
-    if (total === 0 || cart.length === 0) {
+        if (total === 0 || cart.length === 0) {
         swal("Oops!", "No tienes productos en tu carrito!", "error");
-    } else {
+        } 
+        else {
         $('.cuotas').append(`
         <select name="cuota" id="select">
         <option value="$${total} en 1 pago de ${total}" class="option1" name="option"> 1 pago de $${total} sin interés </option>
@@ -166,13 +165,36 @@ $(".button-checkout").one( "click", function (e) {
         );
     }
 
-});
+    $(".containergrid").css("filter", "blur(0px)");
+    $(".form").slideDown(1500);
 
+    dataPedido();
+
+    $('.resumen-pedido').css("display", "block");
+    $('.resumen-pedido').animate({
+                                fontSize: "15px",
+                                },
+                                2000, function(){console.log('fin')
+                            });
+
+});
 
 
 $('.submit').on("click", function(e) {
 
     e.preventDefault();
+    let form = document.getElementById('form')
+    let data = new FormData(form);
+    let infoData = JSON.stringify(data.get('username'));
+
+    $.ajax ({
+    method: 'POST',
+    url : 'https://jsonplaceholder.typicode.com/posts',
+    data: infoData,
+    success: function() {
+        console.log('Datos enviados correctamente');
+        }
+    })
 
     let name = document.getElementById('input-name').value;
     JSON.stringify(sessionStorage.setItem('name', name));
@@ -182,34 +204,26 @@ $('.submit').on("click", function(e) {
     sessionStorage.setItem('option', option);
     let cvv = document.getElementById('input-cvv').value;
 
-
     if (name === '') {
         swal("Oops!", "Ingrese un nombre valido", "error");
     }
     else if (email === '') {
         swal("Oops!", "Ingrese un mail valido", "error");
-
     }
     else if (cvv.length !== 3) {
-
         swal("Oops!", "Ingrese un codigo de seguridad valido", "error");
     } else {
-
         swal({
             title: "PAGO APROBADO #076!",
             text: 'Gracias por tu compra, ' + JSON.parse(sessionStorage.getItem('username').toUpperCase()) + '!\n' + 'En unos minutos llegara un comprobante a ' + sessionStorage.getItem('email'),
             icon: "success",
             button: "OK",
         });
-
     }
-
 });
-
 
 let emptyButton = document.querySelector('.empty-cart');
 emptyButton.addEventListener('click', function() {
-
 
     accumulator = 0;
     renderAccumulator(accumulator);
@@ -217,16 +231,14 @@ emptyButton.addEventListener('click', function() {
     if (cart.length === 0) {
 
         swal({
-
             title: "Oops!",
             text: "No tienes items en tu carrito",
             icon: "error",
             button: "OK",
-
         })
         
         $(".containercart").slideUp(1000);
-        $(".containergrid").css("filter", "none")
+        $(".containergrid").css("filter", "blur(0px)")
         clicks--;;
         cart = [];
         accumulator = 0;
@@ -234,84 +246,77 @@ emptyButton.addEventListener('click', function() {
 
     } else {
             cart = [];
-
             let td = document.getElementsByTagName('td');
-    
             for (const td1 of td) {
                     td1.parentNode.remove();
-            }
-
+                }
             accumulator = 0;
             renderAccumulator(accumulator);
+        }
     }
-});
-
+);
 
 let clicks = 0;
 
-$('.cartlogo').on('click', function() 
+$('.cartlogo').on('click', function() {
 
-{
-
-    if (clicks===0){
+    if (clicks === 0) {
         $(".containercart").slideDown(1000);
         $(".containercart").css("position", "absolute");
         $(".containercart").css("z-index", "1");
         $(".containergrid").css("filter", "blur(5px)");
-        clicks++;
-        // $(".containergrid").css("position", "relative")
+        clicks ++;
     } else {
         $(".containercart").slideUp(1000);
         $(".containergrid").css("filter", "none")
-        clicks--;
+        clicks --;
+        }
     }
-});
-    
+);
+
 
 $(document).ready(function(){
-    // Add smooth scrolling to all links
+    // Añadir smooth scrolling a todos los links
     $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
+      //
+    if (this.hash !== "") {
+        //
         event.preventDefault();
-  
-        // Store hash
+        //
         var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-  
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
-  });
 
+        $('html, body').animate({
+        scrollTop: $(hash).offset().top
+        }, 800, function(){
+        window.location.hash = hash;
+        });
+      } // Termina if
+    });
+});
 
 $('#cartMenu').on('click', function(e){
 
-    if (clicks===0){
+    if (clicks === 0){
+        //Clickear carrito, fondo borroso.
         $(".containercart").slideDown(1000);
         $(".containercart").css("position", "absolute");
         $(".containercart").css("z-index", "1");
         $(".containergrid").css("filter", "blur(5px)");
-        clicks++;
-        // $(".containergrid").css("position", "relative")
+        clicks ++;
     } else {
-
+        // Deshacerfondo borroso y deslizar hacia arriba.
         $(".containercart").slideUp(1000);
-        $(".containergrid").css("filter", "none")
-        clicks--;
+        $(".containergrid").css("filter", "blur(0px)")
+        clicks --;
     }
 });
 
-
-
-
-
+$('.vendedor').one('click',function() {
+    const urlGET = 'https://hp-api.herokuapp.com/api/characters';
+    $.get (urlGET, function(answer, estado) {
+        if (estado === "success") {
+            let miData = answer;
+                $('.vendedor-asignado').append(`<p>${miData[1].name} es tu vendedor asignado</p>`);
+        }
+    })
+})
